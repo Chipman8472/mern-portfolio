b2f43477d7676bdaaf4ed7f04a5f4d7fb344cd89
JustinChipman8472 <157505256+Chipman8472@users.noreply.github.com>
2025-10-16 15:44:17 -0400

    Add SpaceBg component for global theme background

---
+++ client/src/components/SpaceBg.jsx
@@ -0,0 +1,9 @@
+export default function SpaceBg() {
+  return (
+    <>
+      <div className="space-stars" />
+      <div className="space-stars2" />
+      <div className="space-stars3" />
+    </>
+  );
+}
